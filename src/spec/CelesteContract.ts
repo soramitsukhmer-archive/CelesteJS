75b8863929ab6845bc20233b6a1e016a6ad76e41
Socheat Sok <alex@socheat.net>
2020-07-13 17:38:58 +0700

    fix(core): fix CelesteContract method name

--- src/spec/CelesteContract.ts
+++ src/spec/CelesteContract.ts
@@ -2,6 +2,6 @@ import { FunctionalityContract } from './FunctionalityContract'
 import { AccessRightContract } from './AccessRightContract'
 
 export interface CelesteContract {
-    addFunctionity(functionality: FunctionalityContract): this;
+    addFunctionality(functionality: FunctionalityContract): this;
     addAccessRight(functionalityID: Number, rights: AccessRightContract): this;
 }
