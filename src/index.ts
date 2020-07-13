ebfac7bd4688334ac00ea8326d2104f59b55f0db
Socheat Sok <alex@socheat.net>
2020-07-13 17:11:02 +0700

    fix(core): fix syntax in src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export type { AccessRightContract, AccessRightType } from './spec/AccessRightContract';
 export type { FunctionalityContract, FunctionalityType } from './spec/FunctionalityContract';
 
-export { default as AccessRight } from './core/AccessRight'
-export { default as Functionality } from './core/Functionality'
+export { default as AccessRight } from './core/AccessRight';
+export { default as Functionality } from './core/Functionality';
