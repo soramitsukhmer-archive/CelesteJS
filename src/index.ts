f5c7c463fac4c77d2a088b920122f18072e3ffbd
Socheat Sok <alex@socheat.net>
2020-07-14 14:19:52 +0700

    feat(core): add Celeste core to default export in src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
-export type { CelesteContract } from './spec/CelesteContract'
-export type { AccessRightContract, AccessRightType } from './spec/AccessRightContract';
-export type { FunctionalityContract, FunctionalityType } from './spec/FunctionalityContract';
-
 export { default as AccessRight } from './core/AccessRight';
 export { default as Functionality } from './core/Functionality';
+
+import Celeste from './Celeste'
+
+/**
+ * Default Celeste Export
+ */
+export default Celeste;
