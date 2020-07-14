d1e077bfa7f2becf33ca8b0b23f68c2e2e5421c9
Socheat Sok <alex@socheat.net>
2020-07-14 09:38:03 +0700

    feat(core): update specs interface for Celeste

--- src/spec/CelesteContract.ts
+++ src/spec/CelesteContract.ts
@@ -4,4 +4,6 @@ import { AccessRightContract } from './AccessRightContract'
 export interface CelesteContract {
     addFunctionality(functionalityID: Number): this;
     addAccessRights(functionalityID: Number, rightsID: Number): this;
+    findFunctionalityById(id: Number): {index: Number, item: FunctionalityContract };
+    findRightsById(id: Number): {index: Number, item: AccessRightContract };
 }
