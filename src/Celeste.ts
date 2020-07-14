9dc2861da6b58938bcf81ef5d2737bb5c8e1c2bc
Socheat Sok <alex@socheat.net>
2020-07-14 09:51:44 +0700

    feat(core): update Celeste core

--- src/Celeste.ts
+++ src/Celeste.ts
@@ -3,8 +3,8 @@ import { FunctionalityContract } from './spec/FunctionalityContract'
 import { AccessRightContract } from './spec/AccessRightContract'
 
 export default class Celeste implements CelesteContract {
-    functionalities: Array<FunctionalityContract> = [];
-    rights: Array<AccessRightContract> = [];
+    private functionalities: Array<FunctionalityContract> = [];
+    private rights: Array<AccessRightContract> = [];
 
     /**
      * Create a new instance of Celeste
@@ -20,7 +20,13 @@ export default class Celeste implements CelesteContract {
      * Add new functionality
      * @param functionality
      */
-    addFunctionality(functionality: FunctionalityContract) {
+    addFunctionality(functionalityID: Number) {
+        const functionality = this.findFunctionalityById(functionalityID);
+
+        if ([functionality.index].includes(-1)) {
+            // throw func not found
+        }
+
         return this
     }
 
@@ -29,7 +35,54 @@ export default class Celeste implements CelesteContract {
      * @param functionalityID
      * @param rights
      */
-    addAccessRights(functionalityID: Number, rights: AccessRightContract) {
+    addAccessRights(functionalityID: Number, rightsID: Number) {
+        const functionality = this.findFunctionalityById(functionalityID)
+        const rights = this.findRightsById(rightsID)
+
         return this
+    }
+
+    /**
+     * Find functionality by ID
+     * @param id
+     */
+    findFunctionalityById(id: Number) {
+        const index = this.functionalities.findIndex(item => item.id === id);
+        if (index === -1) this.throwFunctionalityNotFoundException();
+        const item = this.functionalities[index];
+        return { index, item };
+    }
+
+    /**
+     * Find rights by ID
+     * @param id
+     */
+    findRightsById(id: Number) {
+        const index = this.rights.findIndex((item) => item.id === id);
+        if (index === -1) this.throwAccessRightsNotFoundException()
+        const item = this.rights[index];
+        return { index, item };
+    }
+
+    /**
+     * Throw functionality not found exception
+     */
+    private throwFunctionalityNotFoundException() {
+        this.throwError('Functionality not found.')
+    }
+
+    /**
+     * Throw access rights not found exception
+     */
+    private throwAccessRightsNotFoundException() {
+        this.throwError("AccessRights not found.");
+    }
+
+    /**
+     * Throw error exception
+     * @param message
+     */
+    private throwError(message: String) {
+        throw new Error(`[Celeste]: ${message}`);
     }
 }
